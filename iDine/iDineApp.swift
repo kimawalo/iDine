0b03c1fb635ff9e639951814e826b13b78f54c50
kimawalo <kimawalo@gmail.com>
2020-11-10 23:15:25 +0200

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Jamshid Azizov on 10.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
